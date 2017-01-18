13562f606452db103fc734d5cf886a089efefffb
ace <andrew.emil@gmail.com>
2017-01-18 00:27:39 -0500

    Added messaging for enemy controller, need to figure out how to do ids

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -25,14 +25,32 @@ public class EnemyController : MonoBehaviour {
 
 	void TakeDamage (int damageAmount) {
 		health = health - damageAmount;
+		messageBox.SendMessage (CreateDamageMessage (damageAmount));
 		if (health < 0) {
 			Die ();
 		}
 	}
 
+	Message CreateDamageMessage(int damangeAmmount) {
+		Message message = new Message ();
+		message.action = "" + damangeAmmount;
+		message.type = "damage";
+		return message;
+	}
+
+	Message CreateDeadMessage() {
+		Message message = new Message ();
+		message.type = "die";
+		message.action = "die";
+		return message;
+	}
+
+
 	void Die() {
 		//TODO
 		Debug.Log("I am dead");
+		messageBox.SendMessage (CreateDeadMessage());
+		gameObject.SetActive (false);
 	}
 
 	void OnTriggerEnter(Collider other) {
